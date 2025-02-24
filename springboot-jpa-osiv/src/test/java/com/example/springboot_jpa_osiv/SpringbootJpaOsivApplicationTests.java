bf76e64d0cace7f5dc87f049bbe5587e3b731ded
fromitive <fromitive@gmail.com>
2025-02-24 11:22:46 +0900

    init project

---
+++ springboot-jpa-osiv/src/test/java/com/example/springboot_jpa_osiv/SpringbootJpaOsivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_jpa_osiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaOsivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
